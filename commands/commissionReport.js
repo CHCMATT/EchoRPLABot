dc6f1022b0342ade8e1c3e1bbf6cd1030b8ec2f1
Matthew Anthony <misterbaboo@gmail.com>
2023-04-21 22:08:50 -0400

    Add report command

---
+++ commands/commissionReport.js
@@ -0,0 +1,16 @@
+var { PermissionsBitField } = require('discord.js');
+var commissionCmds = require('../commissionCmds.js');
+
+module.exports = {
+	name: 'commissionreport',
+	description: 'Manually runs the commission report for the Management team',
+	async execute(interaction) {
+		if (interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+			commissionCmds.weeklyReport(interaction.client);
+			await interaction.reply({ content: `Successfully ran the weekly report.`, ephemeral: true });
+		}
+		else {
+			await interaction.reply({ content: `:x: You must have the \`Administrator\` permission to use this function.`, ephemeral: true });
+		}
+	},
+};
