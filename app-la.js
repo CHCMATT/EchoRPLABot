58a73d44b41b1c26602131aca18b808ccdd04d58
Matthew Anthony <misterbaboo@gmail.com>
2023-05-05 22:09:00 -0400

    Fix schedule

--- app-la.js
+++ app-la.js
@@ -18,7 +18,7 @@ client.login(process.env.TOKEN);
 var fileParts = __filename.split(/[\\/]/);
 var fileName = fileParts[fileParts.length - 1];
 
-cron.schedule('08 22 * * FRI', function () { commissionCmds.commissionReport(client, `Automatic`, `\`System\``); }); // runs at 15:00 every Friday
+cron.schedule('00 15 * * FRI', function () { commissionCmds.commissionReport(client, `Automatic`, `\`System\``); }); // runs at 15:00 every Friday
 
 client.once('ready', async () => {
 	console.log(`[${fileName}] The client is starting up!`);
