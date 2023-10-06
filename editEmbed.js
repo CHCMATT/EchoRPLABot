c72d40a718fc26ebb7466b6c683a8af42ae8266d
Matthew Anthony <misterbaboo@gmail.com>
2023-10-06 16:11:11 -0400

    Update stats embed to be more concise

--- editEmbed.js
+++ editEmbed.js
@@ -59,6 +59,80 @@ module.exports.editMainEmbed = async (client) => {
 	}
 };
 
+module.exports.editStatsEmbed = async (client) => {
+	try {
+		let empStats = await dbCmds.currStats();
+
+		let statsDescList = '';
+
+		empStats.sort((a, b) => {
+			let fa = a.charName.toLowerCase(),
+				fb = b.charName.toLowerCase();
+			if (fa < fb) { return -1; }
+			if (fa > fb) { return 1; }
+			return 0;
+		});
+
+		let now = Math.floor(new Date().getTime() / 1000.0);
+		let today = `<t:${now}:d>`;
+
+		for (i = 0; i < empStats.length; i++) {
+			if (empStats[i].carsSold > 0 || empStats[i].weeklyCarsSold > 0 || empStats[i].currentCommission > 0) {
+				statsDescList = statsDescList.concat(`<@${empStats[i].discordId}>\n`);
+			}
+			if (empStats[i].carsSold > 0) {
+				statsDescList = statsDescList.concat(`• ** Cars Sold Overall:** ${empStats[i].carsSold}\n`);
+			}
+			if (empStats[i].weeklyCarsSold > 0) {
+				statsDescList = statsDescList.concat(`• ** Sales This Week:** ${empStats[i].weeklyCarsSold}\n`);
+			}
+			if (empStats[i].currentCommission > 0) {
+				statsDescList = statsDescList.concat(`• ** Weekly Commission:** ${formatter.format(empStats[i].currentCommission)}\n`);
+			}
+
+			statsDescList = statsDescList.concat(`\n`);
+		}
+
+		if (statsDescList == '') {
+			statsDescList = "There is no salesperson statistics to display yet."
+		}
+
+		// theme color palette: https://coolors.co/palette/03045e-023e8a-0077b6-0096c7-00b4d8-48cae4-90e0ef-ade8f4-caf0f8
+
+		let statsEmbed = new EmbedBuilder()
+			.setTitle(`Salesperson Statistics as of ${today}:`)
+			.setDescription(statsDescList)
+			.setColor('90E0EF');
+
+		let statsEmbedId = await dbCmds.readMsgId("statsMsg");
+		let channel = await client.channels.fetch(process.env.PERSONNEL_STATS_CHANNEL_ID)
+		let statsMsg = await channel.messages.fetch(statsEmbedId);
+
+		statsMsg.edit({ embeds: [statsEmbed] });
+
+	} catch (error) {
+		if (process.env.BOT_NAME == 'test') {
+			console.error(error);
+		} else {
+			console.error(error);
+
+			let errTime = moment().format('MMMM Do YYYY, h:mm:ss a');;
+			let fileParts = __filename.split(/[\\/]/);
+			let fileName = fileParts[fileParts.length - 1];
+
+			console.log(`Error occured at ${errTime} at file ${fileName}!`);
+
+			let errorEmbed = [new EmbedBuilder()
+				.setTitle(`An error occured on the ${process.env.BOT_NAME} bot file ${fileName}!`)
+				.setDescription(`\`\`\`${error.toString().slice(0, 2000)}\`\`\``)
+				.setColor('B80600')
+				.setFooter({ text: `${errTime}` })];
+
+			await interaction.client.channels.cache.get(process.env.ERROR_LOG_CHANNEL_ID).send({ embeds: errorEmbed });
+		}
+	}
+};
+
 function addBtnRows() {
 	let row1 = new ActionRowBuilder().addComponents(
 		new ButtonBuilder()
@@ -96,83 +170,4 @@ function addBtnRows() {
 
 	let rows = [row1, row2];
 	return rows;
-};
-
-module.exports.editStatsEmbed = async (client) => {
-	try {
-		let empStats = await dbCmds.currStats();
-
-		let currentDescList = '';
-		let overallDescList = '';
-
-		empStats.sort((a, b) => {
-			let fa = a.charName.toLowerCase(),
-				fb = b.charName.toLowerCase();
-			if (fa < fb) { return -1; }
-			if (fa > fb) { return 1; }
-			return 0;
-		});
-
-		let now = Math.floor(new Date().getTime() / 1000.0);
-		let today = `<t:${now}:d>`;
-
-		for (i = 0; i < empStats.length; i++) {
-			if (empStats[i].carsSold > 0) {
-				overallDescList = overallDescList.concat(`<@${empStats[i].discordId}>\n• **Cars Sold Overall:** ${empStats[i].carsSold}\n\n`);
-			}
-		}
-
-		for (i = 0; i < empStats.length; i++) {
-			if (empStats[i].weeklyCarsSold > 0) {
-				currentDescList = currentDescList.concat(`<@${empStats[i].discordId}>\n• **Cars Sold This Week:** ${empStats[i].weeklyCarsSold}\n• **Weekly Commission:** ${formatter.format(empStats[i].currentCommission)}\n\n`);
-			}
-		}
-
-		if (overallDescList == '') {
-			overallDescList = "There is no overall salesperson data to display yet."
-		}
-
-		if (currentDescList == '') {
-			currentDescList = "There is no weekly salesperson data to display yet."
-		}
-
-		// theme color palette: https://coolors.co/palette/03045e-023e8a-0077b6-0096c7-00b4d8-48cae4-90e0ef-ade8f4-caf0f8
-
-		let overallEmbed = new EmbedBuilder()
-			.setTitle(`Overall Salesperson Stats as of ${today}:`)
-			.setDescription(overallDescList)
-			.setColor('90E0EF');
-
-		let weeklyEmbed = new EmbedBuilder()
-			.setTitle(`Weekly Salesperson Stats as of ${today}:`)
-			.setDescription(currentDescList)
-			.setColor('ADE8F4');
-
-		let statsEmbed = await dbCmds.readMsgId("statsMsg");
-		let channel = await client.channels.fetch(process.env.PERSONNEL_STATS_CHANNEL_ID)
-		let statsMsg = await channel.messages.fetch(statsEmbed);
-
-		statsMsg.edit({ embeds: [overallEmbed, weeklyEmbed] });
-
-	} catch (error) {
-		if (process.env.BOT_NAME == 'test') {
-			console.error(error);
-		} else {
-			console.error(error);
-
-			let errTime = moment().format('MMMM Do YYYY, h:mm:ss a');;
-			let fileParts = __filename.split(/[\\/]/);
-			let fileName = fileParts[fileParts.length - 1];
-
-			console.log(`Error occured at ${errTime} at file ${fileName}!`);
-
-			let errorEmbed = [new EmbedBuilder()
-				.setTitle(`An error occured on the ${process.env.BOT_NAME} bot file ${fileName}!`)
-				.setDescription(`\`\`\`${error.toString().slice(0, 2000)}\`\`\``)
-				.setColor('B80600')
-				.setFooter({ text: `${errTime}` })];
-
-			await interaction.client.channels.cache.get(process.env.ERROR_LOG_CHANNEL_ID).send({ embeds: errorEmbed });
-		}
-	}
 };
