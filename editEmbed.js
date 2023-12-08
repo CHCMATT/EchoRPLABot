312a1fdc92281c9b889f8462f505ff73cbe14ae6
Matthew Anthony <misterbaboo@gmail.com>
2023-12-08 15:07:55 -0500

    Update service outage alert and errString

--- editEmbed.js
+++ editEmbed.js
@@ -52,7 +52,7 @@ module.exports.editMainEmbed = async (client) => {
 
 			let gServUnavailIndc;
 
-			if (errString === 'Error: The service is currently unavailable.') {
+			if (errString === 'Error: The service is currently unavailable.' || errString === 'Error: Internal error encountered.') {
 				gServUnavailIndc = '\`gServUnavailIndc: true\`';
 			} else {
 				gServUnavailIndc = '\`gServUnavailIndc: false\`';
@@ -137,7 +137,7 @@ module.exports.editStatsEmbed = async (client) => {
 
 			let gServUnavailIndc;
 
-			if (errString === 'Error: The service is currently unavailable.') {
+			if (errString === 'Error: The service is currently unavailable.' || errString === 'Error: Internal error encountered.') {
 				gServUnavailIndc = '\`gServUnavailIndc: true\`';
 			} else {
 				gServUnavailIndc = '\`gServUnavailIndc: false\`';
